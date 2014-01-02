0ee656897e0ed25d60c32e23d9f0e777e9f3984d
Jeff Capuano <jcapuano@commandalkon.com>
2014-01-02 13:11:48 -0500

    update e2e test script

--- scripts/e2e-test.bat
+++ scripts/e2e-test.bat
@@ -14,6 +14,14 @@ set BASE_DIR=%~dp0
 cls
 node "%BASE_DIR%\..\node_modules\protractor\bin\protractor" "%BASE_DIR%\..\test\config\protractor-dev.conf.js" %*
 
+@echo Concatenating Test Suite Files
+SET timeStamp=%date:~-10,2%%date:~-7,2%%date:~-4,4%_%time:~0,2%%time:~3,2%%time:~6,2%
+copy /b "%BASE_DIR%\..\test\out\TEST-*.xml" "%BASE_DIR%\..\test\out\e2e_TestResults_%timeStamp%.xml"
+
+@echo Destroying leftover instances of chromedriver.exe
+taskkill /F /IM chromedriver.exe
+
+
 @echo e2e test suite complete
 
 
