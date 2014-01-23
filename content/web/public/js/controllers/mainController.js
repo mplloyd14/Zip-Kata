4a9c607c3215ad45fb62534efd719c947accacdf
rubensbiork <rbiork@commandalkon.com>
2014-01-23 11:00:12 -0500

    fixing errors by that fool jeff

--- content/web/public/js/controllers/mainController.js
+++ content/web/public/js/controllers/mainController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Controllers */
-angular.module('peControllers', ['cai.services'])
+cai.module('peControllers', ['cai.services'])
 	.controller('MainController', function($scope, $log, apiProvider) {
 		$log.log('Loading web main controller');
 		$scope.message = 'Hello world';
