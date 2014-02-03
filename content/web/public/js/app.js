4dee163891ecacf2668e4f2899c36f8f0295d13d
normanbarber <normanebarber@gmail.com>
2014-02-03 11:41:24 -0500

    resetting app.js to match peseed master branch

--- content/web/public/js/app.js
+++ content/web/public/js/app.js
@@ -2,11 +2,11 @@
 
 // Declare app level module which depends on filters, and services
 cai.module('peApp', ['cai.services', 'peControllers', 'ngCookies', 'ngRoute']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.
-      when('/', {    
-		templateUrl: 'hello',
-        controller: 'MainController'
-      });
-    $locationProvider.html5Mode(true);
-}]);
+    config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'hello',
+                controller: 'MainController'
+            });
+        $locationProvider.html5Mode(true);
+    }]);
