12dceec5d116818a019d08a8fa9b618336144a52
Jeff Capuano <jcapuano@commandalkon.com>
2014-05-01 13:37:26 -0400

    role based permissions: part 1

--- content/web/public/js/controllers/simpleViewController.js
+++ content/web/public/js/controllers/simpleViewController.js
@@ -1,7 +1,8 @@
 cai.module('peControllers')
-	.controller('SimpleViewController', ["$scope", function($scope, $http, $window) {
+	.controller('SimpleViewController', ["$scope", "user", function($scope, user) {
     	$scope.input0 = 'Input 0';
     	$scope.input1 = 'Input 1';
     	$scope.input2 = 'Input 2';
     	$scope.input3 = 'Input 3';
+        $scope.role = user.data.product.roles[0];
     }]);
