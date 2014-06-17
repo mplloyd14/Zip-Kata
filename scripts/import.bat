99a38e057dc9bf850bbbba1628281486178f8f68
Jeff Capuano <jcapuano@commandalkon.com>
2014-06-17 16:50:43 -0400

    data and references

--- scripts/import.bat
+++ scripts/import.bat
@@ -6,9 +6,6 @@ set BASE_DIR=%~dp0
 @echo Importing Product definitions...
 mongoimport -h localhost --port 27017 -d mobileconnect -c productDefinitions --drop --jsonArray --stopOnError --file "%BASE_DIR%\..\test\data\productDefinitions.json"
 
-@echo Importing Product instances...
-mongoimport -h localhost --port 27017 -d mobileconnect -c productInstances --drop --jsonArray --stopOnError --file "%BASE_DIR%\..\test\data\productInstances.json"
-
 @echo Importing Companies...
 mongoimport -h localhost --port 27017 -d mobileconnect -c companies --drop --jsonArray --stopOnError --file "%BASE_DIR%\..\test\data\companies.json"
 
