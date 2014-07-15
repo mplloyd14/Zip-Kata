0545558acba66e8a23bac9eec94b9c79c5151d27
Jeff Capuano <jcapuano@commandalkon.com>
2014-07-15 14:17:49 -0400

    restore missing default config settings

--- config/default.js
+++ config/default.js
@@ -1,9 +1,8 @@
 module.exports = {
-	//port  :4000,
+	port : <SUPPLY A PORT FOR THE APPLICATION WEB SERVER>,
 	locales : ['en'],
-	authenticationServer : '',
+	authenticationServer : 'localhost:3000',
 	appBase : '',
-    product_code : '',
 	client : {
 		auth_server : 'authenticationServer',
 		socket_server : 'data.socket.server.host',
@@ -35,19 +34,18 @@ module.exports = {
 	},
 	data : {
 		providers : [
-			'workaround.js'
 		],
 		socket : {
 			server : {
-				//port :  ,
-				//matchOriginProtocol : false,
-				//timeout :
+				host : '',
+				port :  <SUPPLY A PORT FOR THE APPLICATION SOCKET SERVER>,
+				matchOriginProtocol : false,
+				timeout : 10000
 			}
 		},
 		REST : {
 			server : {
-				//port :  4001
-				//port: 8181
+				port :  <SUPPLY A PORT FOR THE APPLICATION REST SERVER>
 			}
 		}
 	},
@@ -55,6 +53,28 @@ module.exports = {
 		loggly : {
 			subdomain : 'cai', // loggly user account subdomain
 			inputToken : '1a21ae55-17e2-47d7-9000-3d7c4e317e20' // loggly input key
+		},
+		server : { // server-side logging parameters
+			levels : ['error', 'warn', 'info', 'debug'], // available log levels
+			transports : { // supported logging transports
+				console : {
+					level : 'info', // maximum level of logged messages
+					colorize : true, // color levels
+					enabled : true // this switch can be used to easily toggle use of a given transport
+				},
+				file : {
+					level : 'warn',
+					enabled : true,
+					filepath : '',
+					filename : '<USE THE SAME NAME AS THE MAIN ENTRY POINT FILE>.app.log',
+					maxFiles : 10,
+					maxsize : 5242880
+				},
+				loggly : {
+					level : 'warn',
+					enabled : false
+				}
+			}
 		},
 		client : { // client-side logging parameters
 			levels : ['error', 'warn', 'info', 'debug'], // available log levels
@@ -85,25 +105,25 @@ module.exports = {
 						// timeout - set seconds before connection times out default:0
 						// noDelay - Disables the Nagle algorithm default:true
 						// keepAlive - Set if keepAlive is used default:0 , which means no keepAlive, set higher than 0 for keepAlive
-						// encoding - �ascii�|�utf8�|�base64� default:null
+						// encoding - ?ascii?|?utf8?|?base64? default:null
 						// }
 					},
 					db : { // mongodb - db options
-						// w - {Number/String, > -1 || �majority� || tag name} the write concern for the operation
-						//    where < 1 is no acknowlegement of write and w >= 1, w = �majority� or tag acknowledges the write
+						// w - {Number/String, > -1 || ?majority? || tag name} the write concern for the operation
+						//    where < 1 is no acknowlegement of write and w >= 1, w = ?majority? or tag acknowledges the write
 						// wtimeout - {Number, 0} set the timeout for waiting for write concern to finish (combines with w option)
 						// fsync - (Boolean, default:false) write waits for fsync before returning
 						// journal - (Boolean, default:false) write waits for journal sync before returning
 						journal : true
 						// native_parser - if true, use native BSON parser
-						// strict - sets strict mode , if true then existing collections can�t be �recreated� etc.
+						// strict - sets strict mode , if true then existing collections can?t be ?recreated? etc.
 						// pk - custom primary key factory to generate _id values (see Custom primary keys).
 						// forceServerObjectId - generation of objectid is delegated to the mongodb server instead of the driver. default is false
 						// retryMiliSeconds - specify the number of milliseconds between connection attempts default:5000
 						// numberOfRetries - specify the number of retries for connection attempts default:3
 						// reaper - enable/disable reaper (true/false) default:false
 						// reaperInterval - specify the number of milliseconds between each reaper attempt default:10000
-						// reaperTimeout - specify the number of milliseconds for timing out callbacks that don�t return default:30000
+						// reaperTimeout - specify the number of milliseconds for timing out callbacks that don?t return default:30000
 						// raw - driver expects Buffer raw bson document, default:false
 						// logger - object specifying error(), debug() and log() functions
 					}
@@ -115,10 +135,10 @@ module.exports = {
 				server : 'mobileconnect'
 			}
 		},
-        collections: {
-            sessions: {
-                database: 'mobileconnect'
-            },
+		collections : {
+			sessions : {
+				database : 'mobileconnect'
+			},
             users: {
                 database: 'mobileconnect'
             },
@@ -128,6 +148,6 @@ module.exports = {
             productDefinitions: {
                 database: 'mobileconnect'
             }
-        }
+		}
 	}
 };
