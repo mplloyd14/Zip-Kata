8119dcd7ecbed5ec855f8d59618fe1e862261e56
Jeff Capuano <jcapuano@commandalkon.com>
2014-07-18 11:35:45 -0400

    futile

--- config/providers/restProvider.js
+++ config/providers/restProvider.js
@@ -3,6 +3,7 @@
 module.exports = {
     'type' : 'REST',
     'services' : {
+        //"/company/:company/product/:product/conference/code/:code" : [
         "/conference/code/:code" : [
 	        {
 	            "method" : "post",
@@ -11,7 +12,8 @@ module.exports = {
                 "external": true
 	        }
 		],
-        
+
+        //"/company/:company/product/:product/team/code/:code" : [
         "/team/code/:code" : [
 	        {
 	            "method" : "post",
@@ -20,7 +22,8 @@ module.exports = {
                 "external": true
 	        }
 		],
-        
+
+        //"/company/:company/product/:product/game/home/:home/visitor/:visitor/date/:date" : [
         "/game/home/:home/visitor/:visitor/date/:date" : [
 	        {
 	            "method" : "post",
