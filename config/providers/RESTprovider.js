9194bb32ee9740f86c9dd19027648e8d8913593a
Todd Kaufman <toddkaufman@gmail.com>
2014-07-21 17:11:51 -0400

    Aligned with new rest server changes

--- config/providers/RESTprovider.js
+++ config/providers/RESTprovider.js
@@ -17,17 +17,6 @@ module.exports = {
                     return "DATA FROM PE_SEED: " + data;
                 });
             }
-        }],
-        "/product/peseed/name/:name" : [
-        {
-            "external": true,
-            "method" : "get",
-            "version" : "0.1.0",
-            handler: function(data) {
-                return Q.fcall(function() {
-                    return "DATA FROM PE_SEED: " + data;
-                });
-            }
         }]
     },
     'requests' : [
