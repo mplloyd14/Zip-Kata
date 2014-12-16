e38e17a134f7e9081dcf08504abcb27899d296b9
Teresa Brant <tbrant@commandalkon.com>
2014-12-16 15:34:49 -0500

    added default examples for missing permissions

--- content/web/public/js/controllers/mainController.js
+++ content/web/public/js/controllers/mainController.js
@@ -2,11 +2,19 @@
 
 /* Controllers */
 cai.module('peControllers', ['cai.services'])
-	.controller('MainController', ['$rootScope', '$scope', '$log', "$http", "$window", 'user', 'core', function($rootScope, $scope, $log, $http, $window, user, core) {
+	.controller('MainController', ['$rootScope', '$scope', '$log', "$http", "$window", 'user', 'core', 'templates', function($rootScope, $scope, $log, $http, $window, user, core, templates) {
 		$log.log('Loading web main controller');
 
         $scope.firstName = user.data.firstName;
         $scope.lastName = user.data.lastName;
+
+		templates.defaultMissingTemplates([
+
+			//default to nothing when canInput5 permission is missing from user
+			{name: 'canInput5Template'},
+			//default to template id "canInput6Default" if canInput6 permission is missing from user
+			{name: 'canInput6Template', reference: 'canInput6Default' }
+		]);
 
         $scope.logout = function() {
             $log.debug('Logout');
