367b25f527a000da134704cf34141a427901e801
Jeff Capuano <jcapuano@commandalkon.com>
2015-01-27 10:12:15 -0500

    a custom client side translation

--- content/admin/js/userSettings.js
+++ content/admin/js/userSettings.js
@@ -2,8 +2,13 @@
 
 //module.controller('commerceSettingsCtrl', ['$scope', '$location', '$log', '$http', '$routeParams', 'users', 'localize', 'config', 'user','companies',
 //function ($scope, $location, $log, $http, $routeParams, users, localize, config,  user, companies) {
-function customadmindemoSettingsCtrl ($scope, $log) {
+function customadmindemoSettingsCtrl ($scope, $log, i18n) {
     $log.debug('Created custom admin user controller');
+
+    i18n.ensureLocaleIsLoaded().then( function() {
+        // Chaining on the promise returned from ensureLocaleIsLoaded() will make sure the translation is loaded.
+        $scope.localMsg = i18n.__('txtLocalMsg');
+    });
 
     //the permissions will be set for the controller to use
     $scope.$on('getUser', function() {
