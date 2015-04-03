83a83dc7ae1933146588af041cd911693c740b6d
Teresa Brant <tbrant@commandalkon.com>
2015-04-03 14:19:26 -0400

    update for route

--- content/mobile/public/js/app.js
+++ content/mobile/public/js/app.js
@@ -1,12 +1,19 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('peApp', ['cai.services', 'peControllers']).
+//angular.module('peApp', ['cai.services', 'peControllers']).
+cai.module('peApp', ['cai.services', 'peControllers', 'ngCookies', 'ngRoute']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
-      when('/mobile', {
+      when('/', {
 		templateUrl: 'hello',
-        controller: 'MainController'
+		controller: 'MobileController'
+
       });
     $locationProvider.html5Mode(true);
 }]);
+
+
+
+
+
