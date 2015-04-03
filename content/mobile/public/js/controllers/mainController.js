f08e7559781916b8d57cf2e8aac943ac8eb12208
Teresa Brant <tbrant@commandalkon.com>
2015-04-03 14:34:54 -0400

    adding mobile

--- content/mobile/public/js/controllers/mainController.js
+++ content/mobile/public/js/controllers/mainController.js
@@ -2,10 +2,10 @@
 
 /* Controllers */
 angular.module('peControllers', ['cai.services'])
+//cai.module('peControllers', ['cai.services'])
 	.controller('MainController', function($scope, $log) {
+	//.controller('MobileController', function($scope, $log) {
 		$log.log('Loading mobile main controller');
-		$scope.message = 'Hello world';
+		$scope.message = 'Hello Mobile world';
 	});
-
-
 
