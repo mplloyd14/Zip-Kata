c6b3400b00f9344e1dd844a79b5da3092e3b7513
Teresa Brant <tbrant@commandalkon.com>
2015-04-03 14:34:22 -0400

    adding mobile

--- content/mobile/public/js/app.js
+++ content/mobile/public/js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-//angular.module('peApp', ['cai.services', 'peControllers']).
 cai.module('peApp', ['cai.services', 'peControllers', 'ngCookies', 'ngRoute']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
