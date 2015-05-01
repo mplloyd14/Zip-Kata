7082e698a3e29481cd639bd1a7faf65a3bd96a47
Teresa Brant <tbrant@commandalkon.com>
2015-05-01 13:45:13 -0400

    adding web

--- content/web/public/js/app.js
+++ content/web/public/js/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-cai.module('peApp', ['cai.services', 'peControllers', 'ngCookies', 'ngRoute']).
+evo.module('peApp', ['evo', 'peControllers', 'ngCookies', 'ngRoute']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
-      when('/', {    
+      when('/', {
 		templateUrl: 'hello',
         controller: 'MainController'
-      });
+      }).otherwise({redirectTo: '/'})
+	;
     $locationProvider.html5Mode(true);
 }]);
