3a634964e2014fc8ea1f715b7c14f41b9d72c0f9
Teresa Brant <tbrant@commandalkon.com>
2015-05-01 13:43:45 -0400

    adding ionic

--- content/mobile/public/js/app.js
+++ content/mobile/public/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
+/*
 angular.module('peApp', ['cai.services', 'peControllers']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
@@ -10,3 +11,17 @@ angular.module('peApp', ['cai.services', 'peControllers']).
       });
     $locationProvider.html5Mode(true);
 }]);
+*/
+
+evo.module('peApp', ['ionic', 'evo', 'peControllers', 'ngCookies']).
+
+	config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+		$stateProvider.state('mobileView', {
+			url: '/',
+			templateUrl: 'hello',
+			controller: 'MainController'
+		});
+
+		$urlRouterProvider.otherwise('/');
+	}])
+;
