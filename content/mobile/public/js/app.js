e8a559740b43a38710d34c181322578e68ee1a66
Teresa Brant <tbrant@commandalkon.com>
2015-05-08 16:32:41 -0400

    added appbase to the link references

--- content/mobile/public/js/app.js
+++ content/mobile/public/js/app.js
@@ -1,19 +1,5 @@
 'use strict';
-
-// Declare app level module which depends on filters, and services
-/*
-angular.module('peApp', ['cai.services', 'peControllers']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.
-      when('/mobile', {
-		templateUrl: 'hello',
-        controller: 'MainController'
-      });
-    $locationProvider.html5Mode(true);
-}]);
-*/
-
-angular.module('peApp', ['ionic', 'evo', 'peControllers', 'ngCookies']).
+evo.module('peApp', ['ionic', 'evo', 'peControllers', 'ngCookies']).
 
 	config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 		$stateProvider.state('mobileView', {
