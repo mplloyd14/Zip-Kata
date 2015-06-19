16423427d2260174737715f012e3971a48e4e56f
Jeff Capuano <jcapuano@commandalkon.com>
2015-06-19 14:09:28 -0400

    add change pwd and logout links

--- content/web/public/js/controllers/mainController.js
+++ content/web/public/js/controllers/mainController.js
@@ -5,8 +5,12 @@ evo.module('peControllers', ['evo'])
 	.controller('MainController', ['$scope', '$log', '$location', 'evoClientData', function($scope, $log, $location, core) {
 		$log.info('Loading web main controller');
         $scope.adminUrl = core.adminUrl;
+        $scope.changeUrl = core.changeUrl;
+        $scope.logoutUrl = core.logoutUrl;
         //$scope.adminUrl = 'http://www.google.com';
         $log.info($scope.adminUrl);
+        $log.info($scope.changeUrl);
+        $log.info($scope.logoutUrl);
     }]);
 
 
