7a402d63473130491f08ff2353b6a638ba449b2c
Jeff Capuano <jcapuano@commandalkon.com>
2015-09-10 16:42:10 -0400

    deploy scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-BASE_DIR=`dirname $0`
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
 LOG_DIR='/var/log/cai'
 BASE_DIR='/opt/cai'
 APP_DIR='high-availability'
@@ -10,12 +13,12 @@ echo "Installing High Availability Server"
 # logging
 mkdir -p $LOG_DIR/$APP_DIR
 #logrotate
-cp $BASE_DIR../scripts/logrotate.cfg /etc/logrotate.d/highavailability
+cp $SCRIPTPATH/logrotate.cfg /etc/logrotate.d/highavailability
 
 #aws cloudwatch
 
 #daemon
-cp $BASE_DIR../scripts/daemon.init /etc/init.d/highavailability
+cp $SCRIPTPATH/daemon.init /etc/init.d/highavailability
 chmod +x /etc/init.d/highavailability
 chkconfig --add highavailability
 service highavailability start
