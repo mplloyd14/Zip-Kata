605350f771081550f3ae19d276e02927fe67b9ba
Dylan <dylanjones000@gmail.com>
2015-09-23 11:07:46 -0400

    added symlinking

--- deploy/post_install.sh
+++ deploy/post_install.sh
@@ -2,8 +2,12 @@
 
 LOG_DIR='/log'
 PE_DIR='/pe-seed'
+SYM_DIR='/opt/cai'
+APP_DIR='/opt/app'
 
 mkdir $LOG_DIR
 mkdir $LOG_DIR/$PE_DIR
+
+ln -f -s $APP_DIR/$PE_DIR $SYM_DIR/$PE_DIR
 
 service pe-seed start
