63138332a69477d3a287ba599386339b3cd9f32e
Dylan <dylanjones000@gmail.com>
2015-09-23 14:31:20 -0400

    tag test

--- deploy/post_install.sh
+++ deploy/post_install.sh
@@ -4,10 +4,11 @@ LOG_DIR='/log'
 PE_DIR='/pe-seed'
 SYM_DIR='/opt/cai'
 APP_DIR='/opt/apps'
+TAG_NAME='pe-seed_test_tag'
 
 mkdir $LOG_DIR
 mkdir $LOG_DIR/$PE_DIR
 
-ln -f -s $APP_DIR/$PE_DIR $SYM_DIR/$PE_DIR
+ln -f -s $APP_DIR/$PE_DIR/$TAG_NAME $SYM_DIR/$PE_DIR
 
 service pe-seed start
