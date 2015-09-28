fdc6bce859425a89822b740ff1d6ca501346a042
Dylan <dylanjones000@gmail.com>
2015-09-28 13:37:13 -0400

    different install dir

--- deploy/post_install.sh
+++ deploy/post_install.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-LOG_DIR='/log'
+LOG_DIR='/loG'
+SERVICE_DIR='deploy_test'
 PE_DIR='pe-seed'
-SYM_DIR='/opt/cai'
-APP_DIR='../apps'
+APP_DIR='/opt/apps'
 TAG_NAME='pe-seed_test_tag'
 
 mkdir $LOG_DIR
 mkdir $LOG_DIR/$PE_DIR
 
-cd /opt/cai
-
-echo "$SYM_DIR/$PE_DIR to $APP_DIR/$PE_DIR/$TAG_NAME"
-ln -f -s $APP_DIR/$PE_DIR/$TAG_NAME $PE_DIR
+echo "$SYM_DIR/$PE_DIR to $APP_DIR/$SERVICE_DIR/$TAG_NAME"
+ln -fsn $APP_DIR/$SERVICE_DIR/$TAG_NAME $PE_DIR
 
 service pe-seed start
