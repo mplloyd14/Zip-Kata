eaa61edc09407f6ab02111be05c7faac0f1580fc
Dylan <dylanjones000@gmail.com>
2015-09-28 13:21:49 -0400

    fixed stupid thing

--- deploy/post_install.sh
+++ deploy/post_install.sh
@@ -3,13 +3,15 @@
 LOG_DIR='/log'
 PE_DIR='pe-seed'
 SYM_DIR='/opt/cai'
-APP_DIR='/opt/apps'
+APP_DIR='../apps'
 TAG_NAME='pe-seed_test_tag'
 
 mkdir $LOG_DIR
 mkdir $LOG_DIR/$PE_DIR
 
+cd /opt/cai
+
 echo "$SYM_DIR/$PE_DIR to $APP_DIR/$PE_DIR/$TAG_NAME"
-ln -f -s $APP_DIR/$PE_DIR/$TAG_NAME $SYM_DIR/$PE_DIR
+ln -f -s $APP_DIR/$PE_DIR/$TAG_NAME $PE_DIR
 
 service pe-seed start
